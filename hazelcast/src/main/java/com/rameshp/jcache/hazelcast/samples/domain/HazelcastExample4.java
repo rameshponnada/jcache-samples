5bcf3a0675c98a2c81588230d302ba29528ea09a
rameshp <rameshp@ivycomptech.com>
2015-05-20 22:41:20 +0530

    hazelcast implementation

---
+++ hazelcast/src/main/java/com/rameshp/jcache/hazelcast/samples/domain/HazelcastExample4.java
@@ -0,0 +1,11 @@
+package com.rameshp.jcache.hazelcast.samples.domain;
+
+/**
+ * Created by Ramesh Ponnada on 5/20/15.
+ */
+public class HazelcastExample4 {
+
+    public static void main(String[] args) {
+
+    }
+}
